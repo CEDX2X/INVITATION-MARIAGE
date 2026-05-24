7f8303f90eb7e7d731ec540fb44885fbd53732a8
CEDX2X <codeversegroup237@gmail.com>
2026-05-24 21:59:46 +0100

    MUSIQUE

--- src/data/weddingData.ts
+++ src/data/weddingData.ts
@@ -49,7 +49,7 @@ export const weddingData: WeddingData = {
   mobileMoneyName: "Elena & Charlie",
   showGiftSection: true,
   romanticPhrase: "L'amour n'est pas seulement un regard que l'on se porte, c'est un regard jeté ensemble dans la même direction.",
-  audioMusicUrl: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", // Musique douce par défaut ou laisser vide
+  audioMusicUrl: "https://drive.google.com/file/d/1uR3z3EEWdFU8-Yt0BhBRtBHxnScF1XvJ/view?usp=drive_link", // Musique douce par défaut ou laisser vide
   heroImage: "https://images.unsplash.com/photo-1511285560929-80b456fea0bc?q=80&w=2669&auto=format&fit=crop",
   welcomeImage: "https://images.unsplash.com/photo-1519741497674-611481863552?q=80&w=2670&auto=format&fit=crop",
   galleryImages: [
